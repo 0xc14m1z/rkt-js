35d634b7f17033d9bfca010ccdd47aa80e29e491
Morgan Dam <0xc14m1z@gmail.com>
2024-12-22 16:50:16 +0100

    Add basic Token classes

---
+++ src/Token.ts
@@ -0,0 +1,94 @@
+export enum TokenKind {
+  OpenParenthesis,
+  ClosedParenthesis,
+  Keyword,
+  Identifier,
+  StringLiteral,
+  NumberLiteral,
+  SingleQuote,
+  Plus,
+  Minus,
+  Asterisk,
+  Slash,
+  Equal,
+  LowerThan,
+  GreaterThan,
+  NewLine,
+  EndOfFile,
+  Illegal,
+}
+
+export abstract class Token {
+  protected _value: string;
+
+  get value(): string {
+    return this._value;
+  }
+
+  protected constructor(
+    readonly kind: TokenKind,
+    value: string,
+  ) {
+    this._value = value;
+  }
+}
+
+function SimpleToken(kind: TokenKind, value: string) {
+  return class extends Token {
+    constructor() {
+      super(kind, value);
+    }
+  };
+}
+
+export class OpenParenthesisToken extends SimpleToken(TokenKind.OpenParenthesis, "(") {}
+export class ClosedParenthesisToken extends SimpleToken(TokenKind.ClosedParenthesis, "(") {}
+export class SingleQuoteToken extends SimpleToken(TokenKind.SingleQuote, "'") {}
+export class PlusToken extends SimpleToken(TokenKind.Plus, "+") {}
+export class MinusToken extends SimpleToken(TokenKind.Minus, "-") {}
+export class AsteriskToken extends SimpleToken(TokenKind.Asterisk, "*") {}
+export class SlashToken extends SimpleToken(TokenKind.Slash, "/") {}
+export class EqualToken extends SimpleToken(TokenKind.Equal, "=") {}
+export class LowerThanToken extends SimpleToken(TokenKind.LowerThan, "<") {}
+export class GreaterThanToken extends SimpleToken(TokenKind.GreaterThan, ">") {}
+
+export class NewLineToken extends SimpleToken(TokenKind.NewLine, "\n") {}
+
+export class IllegalToken extends Token {
+  constructor(value: string) {
+    super(TokenKind.Illegal, value);
+  }
+
+  append(value: string): void {
+    this._value += value;
+  }
+}
+
+export class EndOfFileToken extends SimpleToken(TokenKind.EndOfFile, "") {}
+
+function LiteralToken(kind: TokenKind) {
+  return class extends Token {
+    constructor(value: string) {
+      super(kind, value);
+    }
+  };
+}
+
+export class StringLiteralToken extends LiteralToken(TokenKind.StringLiteral) {}
+export class NumberLiteralToken extends LiteralToken(TokenKind.NumberLiteral) {
+  get numericValue(): number {
+    return Number(this.value);
+  }
+}
+
+export class KeywordToken extends Token {
+  constructor(keyword: string) {
+    super(TokenKind.Keyword, keyword);
+  }
+}
+
+export class IdentifierToken extends Token {
+  constructor(symbol: string) {
+    super(TokenKind.Identifier, symbol);
+  }
+}
