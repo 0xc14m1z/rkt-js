ac1f6d597b7e4599d4a34b8d51c9f40a39e88042
Morgan Dam <0xc14m1z@gmail.com>
2024-12-24 21:45:57 +0100

    Use the new toBeToken matcher

--- src/__tests__/Scanner.test.ts
+++ src/__tests__/Scanner.test.ts
@@ -19,28 +19,28 @@ describe("Scanner", () => {
   it("appends the end of file token", () => {
     const tokens = scan("");
     expect(tokens).toHaveLength(1);
-    expect(tokens.at(-1)).toBeInstanceOf(EndOfFileToken);
+    expect(tokens.at(-1)).toBeToken(EndOfFileToken);
   });
 
   describe("simple tokens", () => {
     it("matches simple tokens", () => {
       const tokens = scan("()'[]");
 
-      expect(tokens[0]).toBeInstanceOf(OpenParenthesisToken);
-      expect(tokens[1]).toBeInstanceOf(ClosedParenthesisToken);
-      expect(tokens[2]).toBeInstanceOf(SingleQuoteToken);
-      expect(tokens[3]).toBeInstanceOf(OpenBracketToken);
-      expect(tokens[4]).toBeInstanceOf(ClosedBracketToken);
+      expect(tokens[0]).toBeToken(OpenParenthesisToken);
+      expect(tokens[1]).toBeToken(ClosedParenthesisToken);
+      expect(tokens[2]).toBeToken(SingleQuoteToken);
+      expect(tokens[3]).toBeToken(OpenBracketToken);
+      expect(tokens[4]).toBeToken(ClosedBracketToken);
     });
 
     it("skips irrelevant white spaces", () => {
       const tokens = scan("(   )\t+\rif\n'");
 
-      expect(tokens[0]).toBeInstanceOf(OpenParenthesisToken);
-      expect(tokens[1]).toBeInstanceOf(ClosedParenthesisToken);
-      expect(tokens[2]).toBeInstanceOf(IdentifierToken);
-      expect(tokens[3]).toBeInstanceOf(IdentifierToken);
-      expect(tokens[4]).toBeInstanceOf(SingleQuoteToken);
+      expect(tokens[0]).toBeToken(OpenParenthesisToken);
+      expect(tokens[1]).toBeToken(ClosedParenthesisToken);
+      expect(tokens[2]).toBeToken(IdentifierToken);
+      expect(tokens[3]).toBeToken(IdentifierToken);
+      expect(tokens[4]).toBeToken(SingleQuoteToken);
     });
   });
 
@@ -48,18 +48,12 @@ describe("Scanner", () => {
     describe("string literals", () => {
       it("matches an empty string", () => {
         const tokens = scan('""');
-
-        const literal = tokens[0];
-        expect(literal).toBeInstanceOf(StringLiteralToken);
-        expect(literal.value).toBe("");
+        expect(tokens[0]).toBeToken(StringLiteralToken, "");
       });
 
       it("matches a non empty string", () => {
         const tokens = scan('"non empty string"');
-
-        const literal = tokens[0];
-        expect(literal).toBeInstanceOf(StringLiteralToken);
-        expect(literal.value).toBe("non empty string");
+        expect(tokens[0]).toBeToken(StringLiteralToken, "non empty string");
       });
     });
 
@@ -67,44 +61,40 @@ describe("Scanner", () => {
       it("matches single digit numbers", () => {
         const tokens = scan("1");
 
-        expect(tokens[0]).toBeInstanceOf(NumberLiteralToken);
+        expect(tokens[0]).toBeToken(NumberLiteralToken, "1");
         const literal = tokens[0] as NumberLiteralToken;
-        expect(literal.value).toBe("1");
         expect(literal.numericValue).toBe(1);
       });
 
       it("matches floating point numbers", () => {
         const tokens = scan("1.234");
 
-        expect(tokens[0]).toBeInstanceOf(NumberLiteralToken);
+        expect(tokens[0]).toBeToken(NumberLiteralToken, "1.234");
         const literal = tokens[0] as NumberLiteralToken;
-        expect(literal.value).toBe("1.234");
         expect(literal.numericValue).toBe(1.234);
       });
 
       it("matches negative numbers", () => {
         const tokens = scan("-1234");
 
-        expect(tokens[0]).toBeInstanceOf(NumberLiteralToken);
+        expect(tokens[0]).toBeToken(NumberLiteralToken, "-1234");
         const literal = tokens[0] as NumberLiteralToken;
-        expect(literal.value).toBe("-1234");
         expect(literal.numericValue).toBe(-1234);
       });
 
       it("matches floating point numbers that starts with zero", () => {
         const tokens = scan("0.1234");
 
-        expect(tokens[0]).toBeInstanceOf(NumberLiteralToken);
+        expect(tokens[0]).toBeToken(NumberLiteralToken, "0.1234");
         const literal = tokens[0] as NumberLiteralToken;
-        expect(literal.value).toBe("0.1234");
         expect(literal.numericValue).toBe(0.1234);
       });
 
       it("stops at non-digit characters", () => {
         const tokens = scan("123)");
 
-        expect(tokens[0]).toBeInstanceOf(NumberLiteralToken);
-        expect(tokens[1]).toBeInstanceOf(ClosedParenthesisToken);
+        expect(tokens[0]).toBeToken(NumberLiteralToken, "123");
+        expect(tokens[1]).toBeToken(ClosedParenthesisToken);
       });
     });
   });
@@ -124,13 +114,9 @@ describe("Scanner", () => {
     ])('matches "%s" as valid identifier', (tested: string) => {
       const tokens = scan(`( ${tested} )`);
 
-      expect(tokens[0]).toBeInstanceOf(OpenParenthesisToken);
-
-      expect(tokens[1]).toBeInstanceOf(IdentifierToken);
-      const identifier = tokens[1] as IdentifierToken;
-      expect(identifier.value).toBe(tested);
-
-      expect(tokens[2]).toBeInstanceOf(ClosedParenthesisToken);
+      expect(tokens[0]).toBeToken(OpenParenthesisToken);
+      expect(tokens[1]).toBeToken(IdentifierToken, tested);
+      expect(tokens[2]).toBeToken(ClosedParenthesisToken);
     });
   });
 
@@ -140,13 +126,9 @@ describe("Scanner", () => {
       (tested: string) => {
         const tokens = scan(`( ${tested} )`);
 
-        expect(tokens[0]).toBeInstanceOf(OpenParenthesisToken);
-
-        expect(tokens[1]).toBeInstanceOf(MacroToken);
-        const identifier = tokens[1] as MacroToken;
-        expect(identifier.value).toBe(tested);
-
-        expect(tokens[2]).toBeInstanceOf(ClosedParenthesisToken);
+        expect(tokens[0]).toBeToken(OpenParenthesisToken);
+        expect(tokens[1]).toBeToken(MacroToken, tested);
+        expect(tokens[2]).toBeToken(ClosedParenthesisToken);
       },
     );
   });
@@ -154,39 +136,26 @@ describe("Scanner", () => {
   describe("comments", () => {
     it("matches a comment", () => {
       const tokens = scan("(+ 1 2) ; this is a comment");
-
-      expect(tokens[5]).toBeInstanceOf(CommentToken);
-      const comment = tokens[5] as CommentToken;
-      expect(comment.value).toBe(" this is a comment");
+      expect(tokens[5]).toBeToken(CommentToken, " this is a comment");
     });
 
     it("matches consecutive comments", () => {
       const tokens = scan("(+ 1 2) ; this is a comment\n;;and this is another one");
 
-      expect(tokens[5]).toBeInstanceOf(CommentToken);
-      expect(tokens[6]).toBeInstanceOf(CommentToken);
-
-      const firstComment = tokens[5] as CommentToken;
-      expect(firstComment.value).toBe(" this is a comment");
-
-      const secondComment = tokens[6] as CommentToken;
-      expect(secondComment.value).toBe(";and this is another one");
+      expect(tokens[5]).toBeToken(CommentToken, " this is a comment");
+      expect(tokens[6]).toBeToken(CommentToken, ";and this is another one");
     });
   });
 
   describe("illegal tokens", () => {
     it("matches unterminated strings", () => {
       const tokens = scan('"unterminated string');
-
-      expect(tokens[0]).toBeInstanceOf(IllegalToken);
-      expect(tokens[0].value).toBe('"');
+      expect(tokens[0]).toBeToken(IllegalToken, '"');
     });
 
     it("matches unnamed macros", () => {
       const tokens = scan("# ; unnamed macro");
-
-      expect(tokens[0]).toBeInstanceOf(IllegalToken);
-      expect(tokens[0].value).toBe("#");
+      expect(tokens[0]).toBeToken(IllegalToken, "#");
     });
   });
 });
